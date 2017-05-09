b6c8d9202e572859d1a5a0a54a72513542214205
Gil Tayar <gil@tayar.org>
2017-05-09 06:53:35 +0300

    server coded

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+let counter = 0
+
+app.put('/counter', (req, res) => {
+  counter = req.body.value
+
+  res.json({counter})
+})
+
+app.post('/counter/increment', (req, res) => {
+  counter += 1
+
+  res.json({ counter })
+})
+
+app.post('/counter/decrement', (req, res) => {
+  counter -= 2
+
+  res.json({ counter })
+})
+
+app.get('/counter', (req, res) => {
+  res.json({ counter })
+})
+
+module.exports = app
